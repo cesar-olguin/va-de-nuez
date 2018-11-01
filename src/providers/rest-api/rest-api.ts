083af92e12be1d08546b2d91ac656b01c6576105
Olguín Morales César Armando <olgcesar@gmail.com>
2018-11-01 10:51:03 -0600

    noticias generales agregadas

--- src/providers/rest-api/rest-api.ts
+++ src/providers/rest-api/rest-api.ts
@@ -11,7 +11,62 @@ import { Injectable } from '@angular/core';
 export class RestApiProvider {
 
   constructor(public http: HttpClient) {
-    console.log('Hello RestApiProvider Provider');
+    //console.log('Hello RestApiProvider Provider');
   }
 
+  baseURL="http://vadenuez.info/wp/wp-json/wp/v2/";
+
+  getTodasLasNoticias(){
+    return new Promise(resolve => {
+      this.http.get(this.baseURL + 'posts').subscribe(data => {
+        resolve(data);
+      }, err =>{
+        console.log(err);
+      });
+    });
+  }
+
+  getCategorias(){
+    return new Promise(resolve => {
+      this.http.get(this.baseURL + 'categories').subscribe(data => {
+        resolve(data);
+      }, err => {
+        console.log(err);
+      });
+    });
+  }
+
+  getNoticias(idCategoria){
+    return new Promise(resolve => {
+      this.http.get(this.baseURL + 'posts?categories='+ idCategoria).subscribe(data => {
+        resolve(data);
+      }, err => {
+        console.log(err);
+      });
+    });
+  }
+
+  getNoticiaId(idNoticia){
+    return new Promise(resolve => {
+      this.http.get(this.baseURL + 'posts/'+ idNoticia).subscribe(data => {
+        resolve(data);
+      }, err => {
+        console.log(err);
+      });
+    });
+  }
+
+  getFotos(){
+    return new Promise(resolve => {
+      this.http.get(this.baseURL + 'media').subscribe(data => {
+        resolve(data);
+      }, err => {
+        console.log(err);
+      });
+    });
+  }
+
+  getUsuarios(){
+    return this.http.get('https://randomuser.me/api/?results=100');
+  }
 }
