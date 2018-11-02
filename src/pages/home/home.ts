fafc596aed406dec24978c5a8b7da8e179ffc5cf
Olguín Morales César Armando <olgcesar@gmail.com>
2018-11-01 18:33:25 -0600

    imagenes agregadas

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,15 +10,44 @@ import { NoticiaPage } from '../noticia/noticia';
 export class HomePage {
 
   todasLasNoticias: any;
+  todasLasImagenes: any;
+  codigoImagen;
+  codigoImagenNoticia;
+  imagenNoticia: any;
+  imagen;
 
   constructor(public navCtrl: NavController, public restApi: RestApiProvider) {
     this.restApi.getTodasLasNoticias().then((data) => {
       this.todasLasNoticias = data;
+
+      let obj = JSON.parse(JSON.stringify(data));
+      
+      for (var i = 0; i < obj.length; i++) {
+        this.codigoImagenNoticia = obj[i];
+    
+        this.restApi.getNoticiaIdFoto(this.codigoImagenNoticia.featured_media).then((data) => {
+          let obj = JSON.parse(JSON.stringify(data));
+          for (var i = 0; i < obj.length; i++) {
+            this.imagen = obj[i];
+
+            if(this.codigoImagenNoticia.featured_media == this.imagen.id){
+              console.log("Bien");
+              
+              this.imagenNoticia = this.imagen.guid.rendered;
+            }
+          }
+        });
+      }
+    });
+
+    this.restApi.getTodasLaNoticiasImagenes().then((data) => {
+      this.todasLasImagenes = data;
+      let obj = JSON.parse(JSON.stringify(data));
     });
   }
 
-  noticiaSeleccionada(noticia){
-    this.navCtrl.push(NoticiaPage,{
+  noticiaSeleccionada(noticia) {
+    this.navCtrl.push(NoticiaPage, {
       id: noticia.id
     });
   }
