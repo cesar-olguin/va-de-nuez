f73173e308038e7504020a6c4c7e3f411d292120
César Armando Olguín Morales <olgcesar@gmail.com>
2018-11-06 16:31:37 -0600

    noticias con imagen

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,49 +8,17 @@ import { NoticiaPage } from "../noticia/noticia";
   templateUrl: "home.html"
 })
 export class HomePage {
+
   todasLasNoticias: any;
-  todasLasImagenes: any;
-  codigoNoticia_Imagen;
-  codigoImagen_Noticia;
-  imagenNoticia: any;
-  imagen;
 
   constructor(public navCtrl: NavController, public restApi: RestApiProvider) {
-    this.juntarNoticia_Imagen();
+    this.cargarNoticias();
   }
 
   cargarNoticias() {
     this.restApi.getTodasLasNoticias().then(data => {
       this.todasLasNoticias = data;
-
-      // let obj = JSON.parse(JSON.stringify(data));
-
-      // for (var i = 0; i < obj.length; i++) {
-      //   this.codigoImagenNoticia = obj[i];
-
-      //   this.restApi.getNoticiaIdFoto(this.codigoImagenNoticia.featured_media).then((data) => {
-      //     let objImagen = JSON.parse(JSON.stringify(data));
-      //     for (var x = 0; x < objImagen.length; x++) {
-      //       this.imagen = objImagen[x];
-
-      //     }
-      //     this.imagenNoticia = this.imagen.guid.rendered;
-      //   });
-      // }
     });
-  }
-
-  cargarImagenes() {
-    this.restApi.getTodasLaNoticiasImagenes().then(data => {
-      this.todasLasImagenes = data;
-    });
-  }
-
-  juntarNoticia_Imagen() {
-    this.cargarNoticias();
-    this.cargarImagenes();
-
-  
   }
 
   noticiaSeleccionada(noticia) {
