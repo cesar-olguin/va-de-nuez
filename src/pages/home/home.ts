8aeae5bb3430594f7ca9450a9187fe652f13dfc0
César Armando Olguín Morales <olgcesar@gmail.com>
2018-11-07 11:58:56 -0600

    scroll infinito noticias generales

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,24 +1,47 @@
-import { Component } from "@angular/core";
-import { NavController } from "ionic-angular";
+import { Component, ViewChild } from "@angular/core";
+import { NavController, Content } from "ionic-angular";
 import { RestApiProvider } from "../../providers/rest-api/rest-api";
 import { NoticiaPage } from "../noticia/noticia";
+//import { HttpClient } from "@angular/common/http";
+
 
 @Component({
   selector: "page-home",
   templateUrl: "home.html"
 })
+
 export class HomePage {
-
+  @ViewChild(Content) content: Content;
   todasLasNoticias: any;
+  noticiasPagina: any;
+  pagina = 1;
+  paginasMaximas = 500;
 
-  constructor(public navCtrl: NavController, public restApi: RestApiProvider) {
+  constructor(
+    public navCtrl: NavController,
+    public restApi: RestApiProvider,
+    //private httpClient: HttpClient
+  ) {
     this.cargarNoticias();
   }
 
-  cargarNoticias() {
-    this.restApi.getTodasLasNoticias().then(data => {
+  cargarNoticias(infiniteScroll?) {
+    this.restApi.getTodasLasNoticias(this.pagina).then(data => {
       this.todasLasNoticias = data;
+      if (infiniteScroll) {
+        this.todasLasNoticias = this.todasLasNoticias.concat(data);
+        infiniteScroll.complete();
+      }
     });
+  }
+
+  cargarMasNoticias(infiniteScroll) {
+    this.pagina++;
+    this.cargarNoticias(infiniteScroll);
+    //this.content.scrollToTop();
+    if (this.pagina === this.paginasMaximas) {
+      infiniteScroll.enable(false);
+    }
   }
 
   noticiaSeleccionada(noticia) {
